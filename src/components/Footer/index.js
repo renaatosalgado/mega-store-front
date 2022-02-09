b402fb45cb42f10ef96ef4f4951cbc8039a5de40
Lucas <lucasazzollinivieira@gmail.com>
2022-02-09 20:24:06 -0300

    feat: footer initial design

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import { Logo } from "../Header/style";
+import { Container } from "./style";
+
+export default function Footer() {
+    return (
+        <Container>
+            <Logo to='/'>MEGASTORE</Logo>
+        </Container>
+    );
+}
