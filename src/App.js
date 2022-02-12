6d110ed6e3ab801554065a144fae7323a5b4424c
Lucas <lucasazzollinivieira@gmail.com>
2022-02-12 15:50:52 -0300

    feat: added all the depts routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login, SignUp, SingleProduct, MainPage, Cart } from "./pages";
 import { AuthProvider } from "./contexts/AuthContext";
 import { GlobalStyle } from "./style/style";
+import BooksDept from "./pages/Departments/Books";
+import CellphoneDept from "./pages/Departments/Cellphones";
+import EletroDept from "./pages/Departments/Eletro";
+import FashionDept from "./pages/Departments/Fashion";
+import GamesDept from "./pages/Departments/Games";
+import HomeThingsDept from "./pages/Departments/HomeThings";
+import InformaticsDept from "./pages/Departments/Informatics";
 
 export default function App() {
   // const product = {
@@ -23,6 +30,13 @@ export default function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/product/:productId" element={<SingleProduct />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/books" element={<BooksDept />} />
+          <Route path="/cellphones" element={<CellphoneDept />} />
+          <Route path="/eletro" element={<EletroDept />} />
+          <Route path="/fashion" element={<FashionDept />} />
+          <Route path="/games" element={<GamesDept />} />
+          <Route path="/home-things" element={<HomeThingsDept />} />
+          <Route path="/informatics" element={<InformaticsDept />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
