eb546bb06ca59763bd79b218a69773bc2e670820
Lucas <lucasazzollinivieira@gmail.com>
2022-02-12 00:32:36 -0300

    chores: added department routes

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const BASE_URL = "https://mega-store-back-driven.herokuapp.com";
 
 function postLogin(body) {
   const promise = axios.post(`${BASE_URL}/login`, body);
@@ -22,11 +22,49 @@ function getSingleProduct(productId) {
 
 function postAddToCart(body, token) {
   const promise = axios.post(`${BASE_URL}/add-to-cart`, body, token);
+
   return promise;
 }
 
 function getItensFromCart(token) {
   const promise = axios.get(`${BASE_URL}/get-cart`, token);
+
+  return promise;
+}
+
+function getCellphones() {
+  const promise = axios.get(`${BASE_URL}/cellphones`);
+
+  return promise;
+}
+
+function getGames() {
+  const promise = axios.get(`${BASE_URL}/games`);
+
+  return promise;
+}
+
+function getEletro() {
+  const promise = axios.get(`${BASE_URL}/eletro`);
+
+  return promise;
+}
+
+function getBooks() {
+  const promise = axios.get(`${BASE_URL}/books`);
+
+  return promise;
+}
+
+function getFashion() {
+  const promise = axios.get(`${BASE_URL}/fashion`);
+
+  return promise;
+}
+
+function getHomeThings() {
+  const promise = axios.get(`${BASE_URL}/home-things`);
+
   return promise;
 }
 
@@ -36,6 +74,12 @@ const api = {
   getSingleProduct,
   postAddToCart,
   getItensFromCart,
+  getCellphones,
+  getGames,
+  getEletro,
+  getFashion,
+  getBooks,
+  getHomeThings,
 };
 
 export default api;
