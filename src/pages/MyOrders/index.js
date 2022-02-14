ed21f097b2e3e06af1093e0d0eba4e96494449dd
Lucas <lucasazzollinivieira@gmail.com>
2022-02-14 10:43:41 -0300

    feat: initial myOrders page with header, footer and title only

---
+++ src/pages/MyOrders/index.js
@@ -0,0 +1,29 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import ScrollButton from "../../components/ScrollTopButton";
+import { DeptHeader, InfinityBorder, SubContainer } from "../Departments/style";
+import { Container, FooterContainer, HeaderContainer } from "../Main/style";
+
+
+export default function MyOrders() {
+
+    return (
+        <>
+            <HeaderContainer>
+                <Header />
+            </HeaderContainer>
+            <Container>
+                <InfinityBorder />
+                <DeptHeader>Meus pedidos</DeptHeader>
+                <InfinityBorder />
+                <SubContainer>
+                    asdasd
+                </SubContainer>
+            </Container>
+            <ScrollButton />
+            <FooterContainer>
+                <Footer />
+            </FooterContainer>
+        </>
+    );
+}
